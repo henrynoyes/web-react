152e3106c0d54d07fd2fa681326ac40af9a5cafa
Henry Noyes <noyes.he@northeastern.edu>
2024-01-14 23:10:56 -0500

    add ico link to head

--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,7 @@ export default function Home() {
 
       <Head>
         <title>{data.name}</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
 
       <MovingHeader
