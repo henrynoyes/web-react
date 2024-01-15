5da64fa0eddcdeca9a5fcaffa00a6306fcf2eb20
Henry Noyes <noyes.he@northeastern.edu>
2024-01-14 22:37:30 -0500

    temp remove gifs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'cloudinary',
-    domains: ['res.cloudinary.com'],
-    path: 'https://res.cloudinary.com/dw0pyfy5l/image/upload'
-  }
 }
 
 module.exports = nextConfig
