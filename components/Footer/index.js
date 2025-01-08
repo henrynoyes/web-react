888df1d8a3c8bb6e4a4c73f70cb8836d0d9b4fcf
Henry Noyes <noyes.he@northeastern.edu>
2025-01-07 22:53:14 -0500

    update footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = ({}) => {
     <>
       <div className="flex justify-center items-center">
         <h1 className="text-sm text-bold my-2 laptop:my-10 p-2 laptop:p-0">
-          © 2024 Henry Noyes
+          © 2025 Henry Noyes
         </h1>
       </div>
     </>
